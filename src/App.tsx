006a488738cadb9899adc055a5b8d88b374c4e6e
YDLongLong <1772653245@qq.com>
2022-01-24 09:52:58 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from 'antd';
+import "./App.css"
+
+export default function App() {
+  return (
+    <div>
+      <h2>App</h2>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
